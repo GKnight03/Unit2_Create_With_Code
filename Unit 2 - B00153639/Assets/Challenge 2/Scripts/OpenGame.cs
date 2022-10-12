7962eb273742f43060632cf5b503f38a577f4e30
GKnight03 <113988595+GKnight03@users.noreply.github.com>
2022-10-12 15:55:40 +0100

    Mhmhm yes
    Unit2_Create_With_Code

---
+++ Unit 2 - B00153639/Assets/Challenge 2/Scripts/OpenGame.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGame : MonoBehaviour {
+
+    public void OpenG(){
+
+        Application.OpenURL("https://gknight03.github.io/Unit2_Create_With_Code/");
+
+    }
+
+
+}
